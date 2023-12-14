37f28c787fc55e5daf3cb67b6887146f974bccdd
Hub_Master <yoda_2005@mail.ru>
2023-12-14 21:28:55 +0700

    added -lighten- func

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ def hex_interpreter(h):
 
 def rgb_interpreter(rgb):
     return '#%02x%02x%02x' % rgb
+
+def lighten(h, percent):
+    color = hex_interpreter(h)
+    goal_1 = color[0] + (255 - color[0]) * percent // 100
+    goal_2 = color[1] + (255 - color[1]) * percent // 100
+    goal_3 = color[2] + (255 - color[2]) * percent // 100
+    print(color)
+    print(goal_1, goal_2, goal_3)
+    color = (goal_1, goal_2, goal_3)
+    return rgb_interpreter(color)
